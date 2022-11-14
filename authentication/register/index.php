1d271705a8164d515a57d764d0bde83e970c471c
Imrich Zeleňák <92807683+Luminarko@users.noreply.github.com>
2022-11-14 11:25:30 +0100

    Update index.php

--- authentication/register/index.php
+++ authentication/register/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jsSHA/2.0.2/sha.js"></script>
-    <script type="text/javascript" src="file.js"></script>
+    <script type="text/javascript" src="passwd.js"></script>
     <title>register</title>
 </head>
 <body>
