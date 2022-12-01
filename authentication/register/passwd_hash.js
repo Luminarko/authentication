ca490474f882b53a2f2ed76c37d1c613470cc2ef
Imrich Zeleňák <92807683+Luminarko@users.noreply.github.com>
2022-12-01 19:11:03 +0100

    Delete passwd_hash.js

--- authentication/register/passwd_hash.js
+++
@@ -1,7 +0,0 @@
-function mySubmit(obj) {
-    var passwd = document.getElementById('passwd');
-    var hash = new jsSHA("SHA-512", "TEXT", {numRounds: 1});
-    hash.update(passwd.value);
-    var hash = hash.getHash("HEX");
-    passwd.value = hash;
-  }
